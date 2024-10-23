63d09ff91b4028ae9d99a59389d9f8317986c940
Ohjackson <wogus3673@naver.com>
2024-10-24 01:18:33 +0900

    1. add .gitignore 2. delete storyboard

---
+++ Sopt_Appstore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sopt_Appstore
+//
+//  Created by Jaehyun Ahn on 10/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
