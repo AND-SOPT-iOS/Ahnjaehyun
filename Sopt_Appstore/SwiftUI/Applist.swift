6eff248fcbd569296ee6d4e84765f99535eb7b38
Ohjackson <wogus3673@naver.com>
2024-11-29 21:49:09 +0900

    [Chore]

--- Sopt_Appstore/SwiftUI/Applist.swift
+++ Sopt_Appstore/SwiftUI/Applist.swift
@@ -11,31 +11,31 @@ struct AppChartView: View {
     
     let appList = App.mockData
     @State private var selectedApp: App?
-
+    
     var body: some View {
         
         NavigationStack {
             
             ScrollView {
                 
-                VStack() {
-                    
-                    ForEach(appList, id: \.ranking) { app in
-                        AppRow(app: app)
-                        
-                    }
-                    .padding(.horizontal, 13)
+                
+                
+                ForEach(appList, id: \.ranking) { app in
+                    AppRow(app: app)
                     
                 }
+                .padding(.horizontal, 13)
+                
+                
             }
             .background(Color.black.ignoresSafeArea())
-         
-                   }
+            
+        }
         .navigationTitle("Chart")
         .navigationBarTitleDisplayMode(.inline)
         .toolbarBackground(Color(red: 22/255, green: 22/255, blue: 24/255).opacity(0.95), for: .navigationBar)
-          
-        }
+        
+    }
     
 }
 
@@ -43,7 +43,7 @@ struct AppChartView: View {
 struct AppRow: View {
     let app: App
     @State private var showDetailView : Bool = false
-
+    
     var body: some View {
         HStack(spacing: 12) {
             Image(uiImage: app.iconImage)
